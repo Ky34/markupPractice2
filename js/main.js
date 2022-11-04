cfbb6a94b0ecaf5fc599876306c863a7f09773c0
Aleksei Kozlov <Ky34.aleksei.com>
2022-11-04 23:03:12 +0300

    mobile adaptation + markup mobile nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 /* подключаем корусель */
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-    dots: false,
-    margin: 48,
-    loop: true
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+          items: 2,
+          margin: 32,
+          dots: false,
+          loop: true,
+          // stagePadding: 180,
+      },
+      // breakpoint from 480 up
+      601 : {
+          items: 2,
+          dots: false,
+          loop: true,
+      },
+      // breakpoint from 768 up
+      1001 : {
+          items: 3,
+          margin: 48,
+          dots: false,
+          loop: true,
+      }
+  }
   })
 });
 
@@ -16,7 +36,7 @@ const video = document.querySelector('#video-file');
 videoWrapper.addEventListener('click', function () { // Слушаем клик по видео
 
   if (video.paused) {          // если видео на паузе
-    videoPicture.classList.add('none'); // скрываем картинку превью
+    videoPicture.classList.add('hidden'); // скрываем картинку превью
     videoBtn.classList.add('none');   // скрываем кнопку
     videoWrapper.classList.remove('video-overlay')  // скрываем оверлей
     video.play();         // воспросизводим видео
